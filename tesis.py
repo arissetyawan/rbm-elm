eedcf35521c92b4b23cc3781a4701963bf280e71
aris setyawan <aris@kiranatama.com>
2018-02-24 23:21:39 +0700

    initial

---
+++ tesis.py
@@ -0,0 +1,10 @@
+import numpy as np
+import sys
+
+
+# Insert the util's path
+sys.path.insert (0, '/home/arissetyawan/APASCA/__THESIS___/ELM/rbm-elm/')
+import ELM as elm
+
+#self, neurons=20, inTrain=None, outTrain=None, W=None, beta=None, init='uniform', batchSize=None)
+ELM()
